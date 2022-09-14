44a104464f6d16861f3f30cfccdb11f9d22551f7
mbawuike3 <mbawuikestanley@gmail.com>
2022-09-14 15:38:39 -0700

    neww

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+"""Defines the Amenity class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-import models
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    class Amenity(BaseModel, Base):
-        __tablename__ = "amenities"
-        name = Column(String(128), nullable=False)
-        # place_amenities = relationship("Place")
-else:
-    class Amenity(BaseModel):
-        '''Create class Amenity'''
-        name = ""
+class Amenity(BaseModel, Base):
+    """Represents an Amenity for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
--- models/review.py
+++ models/review.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from os import getenv
-from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-import models
+"""Defines the Review class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    class Review(BaseModel, Base):
-        """ Review classto store review information """
-        __tablename__ = "reviews"
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=True)
-else:
-    class Review(BaseModel):
-        place_id = ""
-        user_id = ""
-        text = ""
+
+class Review(BaseModel, Base):
+    """Represents a review for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Reviews.
+        text (sqlalchemy String): The review description.
+        place_id (sqlalchemy String): The review's place id.
+        user_id (sqlalchemy String): The review's user id.
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
