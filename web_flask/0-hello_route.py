8c6b3c4c0c943ce55bb034d54646b5179378f860
Mbawuike Stanley <mbawuikestanley@gmail.com>
2022-10-16 11:02:50 +0000

    hello_route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A simple flask script to start up an app
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Return hello hbnb"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
